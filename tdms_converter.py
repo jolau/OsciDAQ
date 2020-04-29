90c606c61812b4917e7453a8965c1ead9c7ddb35
Jonas Lauener <mail@jolau.ch>
2020-04-29 19:50:54 +0200

    added command line

--- tdms_converter.py
+++ tdms_converter.py
@@ -2,26 +2,32 @@ import matplotlib.pyplot as plt
 import numpy as np
 from nptdms import TdmsFile
 from pathlib import Path
+import sys
 
 # dirPath = Path("/Volumes/GoogleDrive/My Drive/WingtraDrive/technical/Hardware/Projects 2020/20200210 DAQ Payload/OsciDAQ/tdms/")
-dirPath = Path("/Users/jolau/Desktop/scope/tdms")
+
+if len(sys.argv) > 1:
+    dirPath = Path(sys.argv[1])
+else:
+    dirPath = Path("/Users/jolau/Desktop/scope/tdms")
 
 tdmsFilePaths = list(dirPath.glob("*.tdms"))
 csvDirPath = dirPath.joinpath("csv")
 csvDirPath.mkdir(exist_ok=True)
 
 for tdmsFilePath in tdmsFilePaths:
-        tdmsFile = TdmsFile.read(tdmsFilePath)
-        data = tdmsFile["data"]
+    tdmsFile = TdmsFile.read(tdmsFilePath)
+    data = tdmsFile["data"]
 
-        time = data['Time (s)']
-        channel1 = data['Channel 1 (V)']
-        channel2 = data['Channel 2 (V)']
+    time = data['Time (s)']
+    channel1 = data['Channel 1 (V)']
+    channel2 = data['Channel 2 (V)']
 
-        stacked = np.column_stack((time, channel1, channel2))
+    stacked = np.column_stack((time, channel1, channel2))
 
-        csvFilePath = csvDirPath.joinpath(tdmsFilePath.stem + ".csv")
-        np.savetxt(csvFilePath, stacked, delimiter=",", header=(time.name + "," + channel1.name + "," + channel2.name), comments="")
-        print("saved file: " + str(csvFilePath))
+    csvFilePath = csvDirPath.joinpath(tdmsFilePath.stem + ".csv")
+    np.savetxt(csvFilePath, stacked, delimiter=",", header=(time.name + "," + channel1.name + "," + channel2.name),
+               comments="")
+    print("saved file: " + str(csvFilePath))
 
 print("Converted all files.")
