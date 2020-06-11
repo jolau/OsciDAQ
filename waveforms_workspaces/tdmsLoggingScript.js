99bee2b5f165f63bd117a1f4cf52ce91ea3c9340
Jonas Lauener <jonas.lauener@wingtra.com>
2020-06-11 11:11:49 +0200

    improved oscidaq workspaces

--- waveforms_workspaces/tdmsLoggingScript.js
+++ waveforms_workspaces/tdmsLoggingScript.js
@@ -1,14 +1,26 @@
+function pad(n, width, z) {
+  z = z || '0';
+  n = n + '';
+  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
+
 const timestamp = new Date();
 const timestampString =   
   timestamp.getUTCFullYear() + "-" + 
-  timestamp.getUTCMonth() + "-" + 
-  timestamp.getUTCDate() + "T" + 
-  timestamp.getUTCHours() + "_" + 
-  timestamp.getUTCMinutes() + "_" +
-  timestamp.getUTCSeconds() + "." +
+  pad(timestamp.getUTCMonth(), 2) + "-" + 
+  pad(timestamp.getUTCDate(), 2) + "T" + 
+  pad(timestamp.getUTCHours(), 2) + "_" + 
+  pad(timestamp.getUTCMinutes(), 2) + "_" +
+  pad(timestamp.getUTCSeconds(), 2) + "." +
   timestamp.getUTCMilliseconds();
 
-Scope.Export("~/Desktop/scope/tdms/acq_" + Index + "_" + timestampString + ".tdms")
+if((Index - 1) % 1000 == 0) {
+    var batchCounter = Math.floor((Index - 1) / 1000)
+    BatchFolder = "batch_" + pad(batchCounter, 2) + "_" + timestampString + "/";
+} else if(typeof BatchFolder == 'undefined') {
+    BatchFolder = "";
+}
+
+Scope.Export("~/Documents/scope/" + BatchFolder + "acq_" + pad(Index, 7) + "_" + timestampString + ".tdms")
 
 Index++
-
